434ce9f55aace1a8ce3d99d91d042277575bd453
Yehonatan Cohen <97702636+yehocoh@users.noreply.github.com>
2023-04-19 10:12:24 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,9 +1,9 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the GNU General Public License version 3.
 
-PRESIGNED_URL=""             # replace with presigned url from email
-MODEL_SIZE="7B,13B,30B,65B"  # edit this list with the model sizes you wish to download
-TARGET_FOLDER=""             # where all files should end up
+PRESIGNED_URL=r"https://dobf1k6cxlizq.cloudfront.net/*?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9kb2JmMWs2Y3hsaXpxLmNsb3VkZnJvbnQubmV0LyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE2ODE4OTE0NDl9fX1dfQ__&Signature=fEFdgJXQmXmtwUcg9uBOxaLltAPq4T-rv9tvF0jso1dzOcW1FDtG276r6bxXs2j3kjsqi3cNw2XsMZn3d4ahnMkftX~O6u3GJH0V4wyq3QMSiLL7lW0F0oTLxzssyz9eOBpJ9sXIHH-dpPEhEnUqmkfMkfdyDgz61QPM4RQ~FSkr9DjeXFxlGJowby4DWfN6FKwm4oyJKeMIKyuc9o-GqijyjXNaVciOSQ6WtoZ0TD0RflrjJFuR8GYnaYFZNNxHSHOhKv4ZVSjxgbuaLf4maTqiBMqKk1pN0DkAfyTb5idWRK-jxZxEf9TsjiXaQNJ681cj4P60blPzhd~AlnTgZA__&Key-Pair-Id=K231VYXPC1TA1R"     # replace with presigned url from email
+MODEL_SIZE="7B"  # edit this list with the model sizes you wish to download
+TARGET_FOLDER="/content/llama"             # where all files should end up
 
 declare -A N_SHARD_DICT
 
